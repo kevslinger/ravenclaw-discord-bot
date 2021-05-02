fdf527aa423ab66a51e0a73d244d2fbc334879a5
kevslinger <siouxman@me.com>
2021-05-01 21:01:05 -0400

    typo in tests

--- tests/test_cipher_race_utils.py
+++ tests/test_cipher_race_utils.py
@@ -3,15 +3,15 @@ from modules.cipher_race import cipher_race_utils, cipher_race_constants
 
 
 @pytest.mark.parametrize("answer,answer_list,correct",
-                         [("and", ["but", "not", "and"], code_constants.CORRECT),
-                          ("but", ["but", "not", "and"], code_constants.CORRECT),
-                          ("not", ["but", "not", "and"], code_constants.CORRECT),
-                          ("the", ["but", "not", "and"], code_constants.INCORRECT),
-                          ("it", ["but", "not", "and"], code_constants.INCORRECT),
-                          ("strength", ["strengthen", "conditioning"], code_constants.INCORRECT),
-                          ("condition", ["strengthen", "conditioning"], code_constants.INCORRECT),
-                          ("strengthen", ["strengthen", "conditioning"], code_constants.CORRECT),
-                          ("conditioning", ["strengthen", "conditioning"], code_constants.CORRECT)
+                         [("and", ["but", "not", "and"], cipher_race_constants.CORRECT),
+                          ("but", ["but", "not", "and"], cipher_race_constants.CORRECT),
+                          ("not", ["but", "not", "and"], cipher_race_constants.CORRECT),
+                          ("the", ["but", "not", "and"], cipher_race_constants.INCORRECT),
+                          ("it", ["but", "not", "and"], cipher_race_constants.INCORRECT),
+                          ("strength", ["strengthen", "conditioning"], cipher_race_constants.INCORRECT),
+                          ("condition", ["strengthen", "conditioning"], cipher_race_constants.INCORRECT),
+                          ("strengthen", ["strengthen", "conditioning"], cipher_race_constants.CORRECT),
+                          ("conditioning", ["strengthen", "conditioning"], cipher_race_constants.CORRECT)
                           ])
 def test_get_answer_result(answer, answer_list, correct):
     result = cipher_race_utils.get_answer_result(answer, answer_list)
