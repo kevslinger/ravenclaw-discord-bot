65130e551f7d1b5d791610e445e34cda3c370225
kevslinger <siouxman@me.com>
2021-08-03 21:03:26 -0400

    Added descriptions for activities, cleaned up delecteactivity a little bit

--- modules/activity_calendar/cog.py
+++ modules/activity_calendar/cog.py
@@ -51,7 +51,8 @@ class ActivityCalendarCog(commands.Cog, name="Activity Calendar"):
         if len(rows_to_announce):
             description = ""
             for row in rows_to_announce:
-                description += f"\n\n[{row[activity_calendar_constants.SHEET_ACTIVITY_COLUMN-1]}]({row[activity_calendar_constants.SHEET_LINK_COLUMN-1]})"
+                description += f"\n\n[{row[activity_calendar_constants.SHEET_ACTIVITY_COLUMN-1]}:]({row[activity_calendar_constants.SHEET_LINK_COLUMN-1]})" \
+                               f"{row[activity_calendar_constants.SHEET_DESCRIPTION_COLUMN-1]}"
             embed = discord.Embed(title=f"24 Hour Warning for the following activities!",
                                   description=description,
                                   color=constants.EMBED_COLOR)
@@ -65,10 +66,11 @@ class ActivityCalendarCog(commands.Cog, name="Activity Calendar"):
     @commands.command(name="addactivity", aliases=["aactivity"])
     async def addactivity(self, ctx: commands.Context, *args):
         """Add an activity to the calendar
-        ~addactivity Dueling Tuesday, May 11, 2021 7pm EDT https://www.reddit.com/r/Dueling"""
+        ~addactivity Dueling "Harry Potter Trivia!" Tuesday, May 11, 2021 7pm EDT https://www.reddit.com/r/Dueling"""
         logging_utils.log_command("addactivity", ctx.channel, ctx.author)
         args = list(args)
         activity = args.pop(0)
+        description = args.pop(0)
         link = args.pop()
 
         user_time = activity_calendar_utils.parse_date(' '.join(args))
@@ -78,13 +80,14 @@ class ActivityCalendarCog(commands.Cog, name="Activity Calendar"):
                               color=constants.EMBED_COLOR)
         embed.add_field(name="Time", value=user_time.strftime(activity_calendar_constants.DISPLAY_DATETIME_FORMAT), inline=False)
         embed.add_field(name="Activity", value=f"{activity}", inline=False)
+        embed.add_field(name="Description", value=f"{description}", inline=False)
         embed.add_field(name="Link", value=link, inline=False)
         await ctx.send(embed=embed)
 
-        self.activity_calendar_sheet.append_row([ctx.guild.name, spreadsheet_time.strftime(activity_calendar_constants.SHEET_DATETIME_FORMAT), activity, link])
+        self.activity_calendar_sheet.append_row([ctx.guild.name, spreadsheet_time.strftime(activity_calendar_constants.SHEET_DATETIME_FORMAT), activity, description, link])
         self.activity_calendar_sheet.sort((activity_calendar_constants.SHEET_TIMESTAMP_COLUMN, 'asc'))
 
-    @commands.command(name="showactivitycalendar", aliases=["showcal", "showcalendar", "activities"])
+    @commands.command(name="showactivitycalendar", aliases=["cal", "showcalendar", "activities"])
     async def showactivitycalendar(self, ctx, timezone: str = activity_calendar_constants.UTC):
         """Displays the current activity calendar
         Can add a timezone to display the times in that timezone
@@ -125,7 +128,8 @@ class ActivityCalendarCog(commands.Cog, name="Activity Calendar"):
 
             if date.day - current_date.day <= 7:
                 description += f"\n\n{activity_calendar_utils.replace_offset(date.strftime(activity_calendar_constants.DISPLAY_DATETIME_FORMAT))}: " \
-                               f"[{row[activity_calendar_constants.SHEET_ACTIVITY_COLUMN-1]}]({row[activity_calendar_constants.SHEET_LINK_COLUMN-1]})"
+                               f"[{row[activity_calendar_constants.SHEET_ACTIVITY_COLUMN-1]}:]({row[activity_calendar_constants.SHEET_LINK_COLUMN-1]})" \
+                               f" {row[activity_calendar_constants.SHEET_DESCRIPTION_COLUMN-1]}"
 
         embed = discord.Embed(title=f"Activity Calendar for Week of {current_date.strftime('%B %d')}",
                               description=description,
@@ -141,21 +145,22 @@ class ActivityCalendarCog(commands.Cog, name="Activity Calendar"):
 
         activity = ' '.join(args)
         result_cells = self.activity_calendar_sheet.findall(ctx.guild.name, in_column=activity_calendar_constants.SHEET_SERVER_COLUMN)
-        print(result_cells)
+        num_deletions = 0
         for cell in result_cells:
             row = self.activity_calendar_sheet.row_values(cell.row)
-            if row[activity_calendar_constants.SHEET_ACTIVITY_COLUMN-1] == activity:
+            if activity in row[activity_calendar_constants.SHEET_ACTIVITY_COLUMN-1]:
                 self.activity_calendar_sheet.delete_row(cell.row)
+                num_deletions += 1
         embed = discord_utils.create_embed()
         embed.add_field(name="Success",
-                        value=f"Deleted {len(result_cells)} instances of {activity} from the calendar.",
+                        value=f"Deleted {num_deletions} instances of {activity} from the calendar.",
                         inline=False)
 
         await ctx.send(embed=embed)
 
     # TODO: should this be in the house points module?
     # TODO: PROBABLY
-    @commands.command(name="submissions")
+    @commands.command(name="submissions", aliases=["submission"])
     async def submissions(self, ctx):
         """Count submissions for homework and extra credit
         ~submissions"""
@@ -169,18 +174,15 @@ class ActivityCalendarCog(commands.Cog, name="Activity Calendar"):
             row = self.activity_calendar_sheet.row_values(cell.row)
             if "Homework:" in row[activity_calendar_constants.SHEET_ACTIVITY_COLUMN-1] or "EC:" in row[activity_calendar_constants.SHEET_ACTIVITY_COLUMN-1]:
                 # TODO: pls
-                if row[activity_calendar_constants.SHEET_LINK_COLUMN-1] not in [link[1] for link in activity_ids]:
-                    activity_ids.append((row[activity_calendar_constants.SHEET_ACTIVITY_COLUMN-1], row[activity_calendar_constants.SHEET_LINK_COLUMN-1]))
+                if row[activity_calendar_constants.SHEET_LINK_COLUMN-1] not in [link[2] for link in activity_ids]:
+                    activity_ids.append((row[activity_calendar_constants.SHEET_ACTIVITY_COLUMN-1], row[activity_calendar_constants.SHEET_DESCRIPTION_COLUMN-1], row[activity_calendar_constants.SHEET_LINK_COLUMN-1]))
 
         HOUSES = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
         description = ""
         for activity in activity_ids:
-            print(activity[0])
-            submission = asyncpraw.models.Submission(self.reddit_client, url=activity[1])
+            submission = asyncpraw.models.Submission(self.reddit_client, url=activity[2])
             comments = await submission.comments()
-            print(comments)
             for comment in comments:
-                print(comment.body)
                 for house in HOUSES:
                     if "submit" in comment.body.lower() and house.lower() in comment.body.lower():
                         if activity[0] not in submission_counts:
@@ -188,7 +190,7 @@ class ActivityCalendarCog(commands.Cog, name="Activity Calendar"):
 
                         submission_counts[activity[0]][HOUSES.index(house)] = len(comment.replies)
             if activity[0] in submission_counts:
-                description += f"\n\n[{activity[0]}]({activity[1]}):\n" \
+                description += f"\n\n[{activity[0]}:]({activity[2]}) {activity[1]}\n" \
                                f"{chr(10).join([f'{house}: {submissions}' for house, submissions in zip(HOUSES, submission_counts[activity[0]])])}"
         embed = discord.Embed(title="Submission counts for HW and ECs",
                               description=description,
