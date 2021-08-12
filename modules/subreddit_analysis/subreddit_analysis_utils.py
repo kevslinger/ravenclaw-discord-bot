2b4e55ac0a7560b4176a243fd2cdc0832ef12dc0
kevslinger <siouxman@me.com>
2021-08-12 15:59:35 -0400

    Hoping these tweaks fix the chrome on heroku otherwise Ill have to remove it

--- modules/subreddit_analysis/subreddit_analysis_utils.py
+++ modules/subreddit_analysis/subreddit_analysis_utils.py
@@ -4,9 +4,8 @@ import os
 
 def get_chromedriver() -> webdriver.Chrome:
     options = webdriver.ChromeOptions()
-    options.add_argument('--no-gpu')
-    options.add_argument('--sandbox')
-    options.add_argument("start-maximized")
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
     options.binary_location = '/app/.apt/usr/bin/google-chrome'
     options.add_experimental_option("prefs", {
         "profile.default_content_setting_values.notifications": 1
