2b95c85f706c5888727ed586cd23ada98e1503e0
kevslinger <siouxman@me.com>
2021-10-06 17:14:22 -0400

    Added channel name to sheet because i think thatll be easier to read. TODO: enable hr, min, day instead of just d/h/m

--- modules/reminder/cog.py
+++ modules/reminder/cog.py
@@ -32,9 +32,10 @@ class ReminderCog(commands.Cog, name="Subreddit Analysis"):
 
         # Server Channel Time Username ID Event
         reminders = self.reminder_tab.get_all_values()
-        for i, row in enumerate(reminders):
-            deadline_time = time_utils.parse_date(row[2],
-                                                  from_tz=row[2].split()[-1],
+        # Skip header row
+        for i, row in enumerate(reminders[1:]):
+            deadline_time = time_utils.parse_date(row[3],
+                                                  from_tz=row[3].split()[-1],
                                                   to_tz=constants.UTC)
             # Check if it's time to remind the person
             if deadline_time.year != utctime.year or deadline_time.month != utctime.month or deadline_time.day != utctime.day \
@@ -46,14 +47,15 @@ class ReminderCog(commands.Cog, name="Subreddit Analysis"):
             idxs_to_delete.append(i)
 
         for row in rows_to_remind:
-            author = self.bot.get_user(int(row[4]))
-            channel = self.bot.get_channel(int(row[1]))
+            author = self.bot.get_user(int(row[5]))
+            channel = self.bot.get_channel(int(row[2]))
 
-            await channel.send(f"{author.mention}, please don't forget to {row[5]}")
+            await channel.send(f"{author.mention}, please don't forget to {row[6]}")
 
         idxs_to_delete = sorted(idxs_to_delete, reverse=True)
+        # enumerate is 0-index, but google is 1-index plus we skip header row so we need +2
         for idx in idxs_to_delete:
-            self.reminder_tab.delete_row(idx+1)
+            self.reminder_tab.delete_row(idx+2)
 
 
     @commands.command(name="remindme", aliases=["remind", "reminder"])
@@ -82,7 +84,8 @@ class ReminderCog(commands.Cog, name="Subreddit Analysis"):
             await ctx.send(embed=embed)
             return
 
-        self.reminder_tab.append_row([ctx.guild.name, str(ctx.channel.id), remind_time.strftime(constants.SHEET_DATETIME_FORMAT),
+        self.reminder_tab.append_row([ctx.guild.name, ctx.channel.name, str(ctx.channel.id),
+                                      remind_time.strftime(constants.SHEET_DATETIME_FORMAT),
                                       ctx.author.name, str(ctx.author.id), ' '.join(args[1:])])
 
         embed = discord_utils.create_embed()
